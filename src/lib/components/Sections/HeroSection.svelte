c386c8283693bf6d3d866da7495bdd79d2b21ec9
Walter Opdam <walterOpdam@yahoo.com.au>
2024-12-07 15:23:24 +1100

    Initial commit

---
+++ src/lib/components/Sections/HeroSection.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+	import { goto } from "$app/navigation";
+	import Button from "$components/Button.svelte";
+
+    function onclick() {
+        goto("/#contact-form");
+    }
+
+</script>
+<section class="hero-section default-margin">
+    <h3>Hello! I'm Wal</h3>
+    <div class="underscore mb-m"></div>
+    <h1>I'm a svelte developer crafting intuitive and performant</h1>
+    <h1 class="dark grey mb-m">web experiences.</h1>
+    <Button {onclick}>Let's talk</Button>
+</section>
+
+<style>
+    .hero-section {
+        padding-top: 60px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<!-- export {default as HeroSection} from './HeroSection.svelte'; -->
+
+<script>
+    import {AboutMeSection, HeroSection} from "$components";
+    // import {HeroSection} from "$lib/components/HeroSection.svelte";
+</script>
+
+<HeroSection />
+<AboutMeSection />
